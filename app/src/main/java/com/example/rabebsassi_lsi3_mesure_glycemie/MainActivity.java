601fd36835c46f5657078f243feb546b72bdf4fe
Rabeb <rabeebsassi@gmail.com>
2023-11-01 14:22:50 +0100

    tp2 mobile

---
+++ app/src/main/java/com/example/rabebsassi_lsi3_mesure_glycemie/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.yosrijemai_lsi3_mesure_glycemie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
